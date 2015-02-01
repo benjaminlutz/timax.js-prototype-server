03a45030ca2fbb61c2b259892fc8dfeb6b3f2f0c
Benjamin Lutz <benjamin.lutz@gmail.com>
2015-02-01 15:48:02 +0100

    example get and post request

--- routes/booking.js
+++ routes/booking.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET bookings listing. */
 router.get('/', function(req, res, next) {
-	res.send({
-		test: 123
-	});
+	var bookings = [
+		{
+			name: 'Book 1',
+			start: '15:40',
+			end: '15:40'
+		},
+		{
+			name: 'Book 2',
+			start: '17:80',
+			end: '19:90'
+		}
+	];
+
+	res.send(bookings);
+});
+
+router.post('/', function(req, res, next) {
+	var booking = req.body;
+
+	console.log(booking);
+	booking.test = true;
+
+	res.send(booking);
 });
 
 module.exports = router;
