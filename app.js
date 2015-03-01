322bd28fc6a18f172878b5e40f15ef9f91019321
Benjamin Lutz <benjamin.lutz@gmail.com>
2015-03-01 16:58:48 +0100

    changed CORS config

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(function(req, res, next) {
 // configure CORS header
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type, Authorization");
     next();
 });
 
